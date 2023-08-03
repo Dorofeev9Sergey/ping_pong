31a43fbfe2400c02e58d1777d33d06462502cad0
Dorofeev9Sergey <dorofeev.serg.009@gmail.com>
2023-08-03 12:35:25 +0300

    добавили изображения и отрисовали их в игровом цикле

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from pygame import *
 window = display.set_mode((700, 500))
 display.set_caption("Пинг понг")
-window.fill((123, 104, 238))
+fon = transform.scale(image.load('background.jpg'), (700, 500))
 
 class GameSprite(sprite.Sprite):
    def __init__(self, image_name, x, y , speed, size_x, size_y):
@@ -30,3 +30,21 @@ class Player(GameSprite):
                 self.rect.y -= self.speed
             if keys_pressed[K_DOWN] and self.rect.y < 470: 
                 self.rect.y += self.speed
+
+rocet = Player('racket.png',50, 50, 1, 40, 70)
+rocet2 = Player('racket.png',650, 50, 1, 40, 70)
+
+game = True
+
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+    window.blit(fon,(0,0))
+    rocet.update()
+    rocet.reset()
+    rocet2.update2()
+    rocet2.reset()
+    time.delay(50)
+    display.update()
+
