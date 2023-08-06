d45e40d18742de10f8ca54d6ebe7e3c33f620bc3
Dorofeev9Sergey <dorofeev.serg.009@gmail.com>
2023-08-06 10:39:18 +0300

    движение мяча

--- main.py
+++ main.py
@@ -31,8 +31,14 @@ class Player(GameSprite):
             if keys_pressed[K_DOWN] and self.rect.y < 470: 
                 self.rect.y += self.speed
 
-rocet = Player('racket.png',50, 50, 1, 40, 70)
-rocet2 = Player('racket.png',650, 50, 1, 40, 70)
+rocet = Player('racket.png',50, 50, 15, 40, 70)
+rocet2 = Player('racket.png',650, 50, 15, 40, 70)
+boll = GameSprite('ball.png', 123, 232, 0, 20, 20)
+
+speed_x = 3
+speed_y = 3
+
+finish = False
 
 game = True
 
@@ -40,11 +46,19 @@ while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
-    window.blit(fon,(0,0))
-    rocet.update()
-    rocet.reset()
-    rocet2.update2()
-    rocet2.reset()
+    if finish == False:
+        window.blit(fon,(0,0))
+        rocet.update()
+        rocet.reset()
+        rocet2.update2()
+        rocet2.reset()
+        boll.rect.x += speed_x
+        boll.rect.y += speed_y
+        boll.reset()
+    if boll.rect.y > 480 or boll.rect.y < 0:
+        speed_y *= -1
+    if sprite.collide_rect(rocet, boll)or sprite.collide_rect(rocet2, boll):
+        speed_x *= -1
     time.delay(50)
     display.update()
 
